70e59dc3399482d2c7cfe3fd71c84e66a20404ef
TXLegend28 <garethzuma28@gmail.com>
2026-02-23 18:23:47 +0200

    Update

---
+++ src/More_Basics.java
@@ -0,0 +1,13 @@
+public class More_Basics {
+
+public static void main(String[] args) {
+    System.out.print("More\nBasics\n");
+    System.out.println("In\nCoding");
+
+    int i = 2;
+    int j = 98;
+
+   IO.println("i + j = " + (i + j));
+
+}
+}
