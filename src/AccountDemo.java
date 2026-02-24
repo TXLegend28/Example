49e2ad8222c059d5746be1baa7786d49e1885196
TXLegend28 <garethzuma28@gmail.com>
2026-02-24 13:02:43 +0200

    New Class for AccountDemo

---
+++ src/AccountDemo.java
@@ -0,0 +1,54 @@
+public class AccountDemo{
+
+  //Instance or global variables 
+
+  public String name;
+  public String surname;
+  public double balance;
+
+  public Account(String name, String surname, double balance){
+
+    this.name = name;
+    this.surname = surname;
+    this.balance = balance;
+  
+  }
+
+//Set Methods
+public String setName(String name){
+  this.name = name;
+
+  }
+public String setSurname(SString surname){
+  this.surname = surname;
+
+  }
+public double deposit(double amount){
+  if(amount > 0)
+  this.balance += ammount;
+
+  }
+//Get Method
+public String getName(){
+  return this.name
+  }
+public String getSurname(){
+  return this.surname
+    }
+  public double getBalance(){
+   return this.balance
+  }
+
+
+  
+}
+
+
+
+
+
+
+
+
+
+
