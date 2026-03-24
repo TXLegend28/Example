0b0dbe2cb8cdaf4bee8b7ffa5f020a810ab593e8
Gareth Zuma <garethzuma28@gmail.com>
2026-03-24 12:59:48 +0200

    ArrayDemo1 from GitHub
    Fix variable name and import statement errors.

---
+++ src/ArrayDemo1.java
@@ -0,0 +1,26 @@
+import java.util.scanner;
+public class ArrayDemo1{
+
+  public static void main(String[] args){
+    double [] monthlyRainfalls = new double[12];
+    Scanner input = new Scanner(System.in);
+
+    double sum = 0.0, annualrainfallAvg;
+
+    for(int i=0; i<monthlyRainfall.length; i++){
+      System.out.println("Enter the rainfall average for month " +(i+1) + " : ");
+      monthlyRainfall[i] = input.nextDouble();
+      sum += monthlyRainfall[i];
+    }
+    annualRainfallAvg = sum/monthlyRainfall.length;
+    System.out.println("The annual average rainfall is: " +annualRainfallAvg);
+
+    for(int i=0; i<monthlyRainfall.length; i++){
+      double difference = monthlyRainfall[i] - annualRainfallAvg;
+      System.out.printf("%3d%20.2f%20.2f%n", (i+1), monthlyRainfall[i], difference);
+    }
+
+
+  }
+
+}
